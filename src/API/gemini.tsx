6122153b2da6909550206ca79e02f4e3e6bcc899
Bamgm14 <46135722+Bamgm14@users.noreply.github.com>
2025-04-21 14:16:30 +1000

    Generic Model Submission for Review type from WrapperObjects

--- src/API/gemini.tsx
+++ src/API/gemini.tsx
@@ -1,25 +1,21 @@
-import {
-  GoogleGenerativeAI,
-  SchemaType,
-  GenerationConfig,
-  ResponseSchema,
-} from "@google/generative-ai";
+import { Review } from "@/pages/WrapperObjects";
+import { GenerateContentResponse, GoogleGenAI, Type } from "@google/genai"
 
 const geminiAPIKey = import.meta.env.VITE_GEMINI_API_KEY;
 
-const genAI = new GoogleGenerativeAI(geminiAPIKey);
+const genAI = new GoogleGenAI(geminiAPIKey);
 
-const schema: ResponseSchema = {
+const schema = {
   description: "Review Summary Schema",
-  type: SchemaType.OBJECT,
+  type: Type.OBJECT,
   properties: {
     summary: {
-      type: SchemaType.STRING,
+      type: Type.STRING,
       description: "Summary of the reviews",
       nullable: false,
     },
     overallSentiment: {
-      type: SchemaType.STRING,
+      type: Type.STRING,
       description:
         "Overall sentiment of the reviews (positive, negative, neutral)",
       nullable: false,
@@ -28,7 +24,7 @@ const schema: ResponseSchema = {
   required: ["summary", "overallSentiment"],
 };
 
-const generationConfig: GenerationConfig = {
+const generationConfig = {
   temperature: 1,
   topP: 0.95,
   topK: 40,
@@ -37,9 +33,24 @@ const generationConfig: GenerationConfig = {
   responseSchema: schema,
 };
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
-  generationConfig: generationConfig,
-});
+const model = async (data: string): Promise<GenerateContentResponse> => {
+  return genAI.models.generateContent({
+      model: "gemini-2.0-flash",
+      config: generationConfig,
+      contents: ["Provide a summary of the reviews and their overall sentiment.", data],
+  });
+}
 
-export default model;
+const generateReviewSummary = async (reviews: Review[]): Promise<{summary: string, overallSentiment: string}> => {
+  const reviewTexts = reviews.map((review) => `Review: ${review.reviewText} \n Rating: ${review.rating}\n\n`).join(" ");
+  const response = await model(reviewTexts);
+  const content: { summary: string; overallSentiment: string } = response?.data[0] as { summary: string; overallSentiment: string };
+  const summary = content.summary;
+  const overallSentiment = content.overallSentiment;
+  return {
+    summary,
+    overallSentiment,
+  };
+}
+
+export { generateReviewSummary };
