9aef72c1a6e97ee0b2deec33915012807d4875b2
Shlok <smar0088@student.monash.edu>
2025-05-11 12:47:52 +1000

    merge

--- src/pages/CustomerDash.tsx
+++ src/pages/CustomerDash.tsx
@@ -7,7 +7,6 @@ import Ratings from "@/components/ui/ratings";
 import { useQuery } from "@tanstack/react-query";
 import { getAllBusinesses, getAllPromotions } from "@/API/RestaurantAPI";
 import { BusinessCard } from "@/components/custom/businessCard";
-import Marquee from "react-fast-marquee";
 import { LuInfo } from "react-icons/lu";
 import Footer from "@/components/Footer";
 import {
@@ -45,7 +44,7 @@ const cuisines: TCuisine[] = [
 
 function CustomerDash() {
   const navigate = useNavigate();
-  const [cuisine, setCuisine] = useState<TCuisine>("All");
+  const [cuisine, setCuisine] = useState<string>("All");
   const [query, setQuery] = useState<string>("");
   const { section } = useParams();
   useEffect(() => {
@@ -83,6 +82,13 @@ function CustomerDash() {
         restaurant={restaurant}
       />
     ));
+
+  const getCategories = getAllBusinessesQuery.data
+    ?.map((restaurant) => restaurant.cuisineType)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  const categories = getCategories?.sort((a, b) =>
+    a.localeCompare(b, undefined, { sensitivity: "base" })
+  );
 
   const getAllPromotionsQuery = useQuery({
     queryFn: () => getAllPromotions(),
@@ -170,24 +176,26 @@ function CustomerDash() {
             <h1 className="text-3xl font-bold">All Restaurants</h1>
           </div>
 
-          <div className="flex flex-row items-center justify-start gap-4 overflow-x-auto w-full py-2 px-1">
-            {cuisines.map((cuisine, index) => (
-              <div
-                key={index}
-                className="border rounded-md px-4 py-2 hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
-                onClick={() => setCuisine(cuisine)}
-              >
-                {cuisine}
-              </div>
-            ))}
+          <div className="flex flex-row items-center justify-start gap-4 w-full py-2 px-1">
             <input
               type="text"
-              className="border rounded-full px-4 py-2 w-full min-w-fit"
+              className="border rounded-full px-4 py-2 w-fit min-w-fit"
               placeholder="Search for a restaurant"
               onChange={(e) => {
                 setQuery(e.target.value);
               }}
             />
+            <div className="flex flex-row items-center justify-start gap-2 w-full overflow-x-auto !hide-scrollbar">
+              {categories?.map((cuisine, index) => (
+                <div
+                  key={index}
+                  className="border rounded-md px-4 py-2 hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800"
+                  onClick={() => setCuisine(cuisine)}
+                >
+                  {cuisine}
+                </div>
+              ))}
+            </div>
           </div>
 
           <div className="flex flex-col justify-center gap-4 w-full">
